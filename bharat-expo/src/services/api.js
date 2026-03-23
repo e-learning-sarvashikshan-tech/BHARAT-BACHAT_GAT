5a201bc03d8fd1fdced34d232dc1a7817f016ab7
sanketdhamne8 <sanketdhamne8@gmail.com>
2026-03-23 11:47:40 +0530

    fix: Added Ngrok bridge URL and EAS config for physical device demo

--- bharat-expo/src/services/api.js
+++ bharat-expo/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // Your confirmed working IP address
-  baseURL: 'http://192.168.0.102:8000/api', 
+  baseURL: 'https://iona-irrelevant-unchaotically.ngrok-free.app/api', 
   headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json',
