641fe1804212f51a1fba2a1d089fa8443bfe77ce
sanketdhamne8 <sanketdhamne8@gmail.com>
2026-03-23 12:14:21 +0530

    fix: Updated Ngrok URL after restart

--- bharat-expo/src/services/api.js
+++ bharat-expo/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // Your confirmed working IP address
-  baseURL: 'https://iona-irrelevant-unchaotically.ngrok-free.app/api', 
+  baseURL: 'https://iona-irrelevant-unchaotically.ngrok-free.dev -> http://localhost:8000 ',
   headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json',
